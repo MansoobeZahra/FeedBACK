2e0f3393fa44a665625f5204d80d83a1745e4e9c
MansoobeZahra <mansoobezehra@gmail.com>
2026-04-24 17:12:19 +0500

    New Commit

---
+++ Default.aspx.vb
@@ -0,0 +1,17 @@
+Public Class DefaultPage
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        If Session("UserID") Is Nothing Then
+            Response.Redirect("~/Login.aspx")
+            Return
+        End If
+        Dim role As String = Session("RoleName").ToString()
+        Select Case role
+            Case "Survey Administrator" : Response.Redirect("~/Admin/Dashboard.aspx")
+            Case "Survey Builder"       : Response.Redirect("~/Builder/Dashboard.aspx")
+            Case "Surveyor"             : Response.Redirect("~/Surveyor/Dashboard.aspx")
+            Case Else                   : Response.Redirect("~/Login.aspx")
+        End Select
+    End Sub
+End Class
